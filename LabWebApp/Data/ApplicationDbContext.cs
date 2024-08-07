be2eca5205506f4e785fd8011b50e56110df8e9b
barrosorr <barroso.5055@gmail.com>
2024-08-07 09:57:53 -0400

    Add project files.

---
+++ LabWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace LabWebApp.Data;
+using Microsoft.EntityFrameworkCore;
+using LabWebApp.Models;
+using System.Collections.Generic;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
+
